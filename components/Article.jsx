c5d0ecb9a936a6045861675ab582a1f66e142c78
dharaneesh-r <investoproweal@gmail.com>
2025-10-01 10:01:43 +0530

    changes made in frontend development for the api calling

---
+++ components/Article.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BasicTable from "./Table";
+
+const Article = () => {
+  return (
+    <>
+      <div className="text-white text-center font-semibold text-3xl mx-10">
+        <BasicTable />
+      </div>
+    </>
+  );
+};
+
+export default Article;
---
+++ components/Mainpage.jsx
@@ -0,0 +1,20 @@
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+const MainPage = () => {
+  const Navigate = useNavigate();
+  return (
+    <div className="bg-black h-screen place-content-center">
+      <div className="text-white  font-semibold text-center text-3xl">
+        DEVOPS FULL COURSE{" "}
+      </div>
+      <div className="text-center my-10">
+        <Button variant="outlined" onClick={() => Navigate("/article")}>
+          ARTICLE
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
